5f270b3020acc92fafb009b54a939de8960f25cb
Brayan Alejandro Retamozo Salazar <xxbrayan@outlook.es>
2024-11-06 10:58:21 -0500

    Add conecction configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    backendUrl: 'https://api.tuservicio.com',
+    apiVersion: 'v1',
+    timeout: 5000,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  backendUrl: 'http://localhost:8089',
+  apiVersion: 'v1',
+  timeout: 5000,
+};
